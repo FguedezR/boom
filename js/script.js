097982f80943ae03120acb5cc9636d477aff9df1
Fernando <fernanguedezr@gmail.com>
2025-12-26 18:43:00 +0100

    js update

--- js/script.js
+++ js/script.js
@@ -1,14 +1,32 @@
 // generar número aleatorio del 1 al 3
 // cuenta atrás de 5 segundos setTimeout()
+// -> vamos a capturar el input en una const userInput
 // después de la cuenta atrás compara el número aleatorio y el del usuario.
 // si coinciden los números ganas si son diferentes pierdes
 // debe salir el número elegido y el número correcto en pantalla
 // no se sabrá que número es hasta pasado 5 segundos.
 
-function getRandomNumber(tomi) {
-    return Math.floor(Math.random() * tomi)
+// numero aleatorio
+function getRandomNumber(max) {
+  return Math.floor(Math.random() * max);
 }
-console.log(getRandomNumber(4))
+console.log(getRandomNumber(4));
+
+const userInput = document.getElementById("userInput");
+
+function countDown(userInput) {
+  if (userInput === getRandomNumber) {
+    alert('has ganado');
+  } else {
+    alert('has perdido');
+  }
+  setTimeout(() => {
+    console.log("Hola, esto funciona");
+  }, 5000);
+}
 
 
 
+/* setTimeout(() => {
+  alert("Hola, esto funciona");
+}, 5000); */
